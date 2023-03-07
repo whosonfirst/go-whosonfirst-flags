034bb710f4e51d187458243fc9c8607a63f1f709
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2023-03-06 16:48:22 -0800

    Update to use whosonfirst/go-whosonfirst-placetypes specification/definitions (#5)
    * start blocking out code to deal with custom placetype specifications
    
    * update to use whosonfirst/go-whosonfirst-placetypes v0.7.0

---
+++ placetypes/whosonfirst_test.go
@@ -0,0 +1,23 @@
+package placetypes
+
+import (
+	"testing"
+)
+
+func TestNewPlacetypeFlag(t *testing.T) {
+
+	tests := []string{
+		"region",
+		"region#whosonfirst://",
+	}
+
+	for _, n := range tests {
+
+		_, err := NewPlacetypeFlag(n)
+
+		if err != nil {
+			t.Fatalf("Failed to create flag for '%s', %v", n, err)
+		}
+	}
+
+}
